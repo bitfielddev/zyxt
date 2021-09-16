87203068367f820448812f6aae18a66d6876cac5
7d <61975820+iiiii7d@users.noreply.github.com>
2021-09-16 23:07:27 +0800

    initial commit

---
+++ src/io/github/iiiii7d/zylang/Main.java
@@ -0,0 +1,9 @@
+package io.github.iiiii7d.zylang;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
