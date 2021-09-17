c4110f08845f754c6f4234179ace29191474b70c
7d <61975820+iiiii7d@users.noreply.github.com>
2021-09-18 00:57:22 +0800

    figuring out packaging, compiling, and resources im new to java lol, apologies

--- src/main/java/io/github/iiiii7d/zylang/Main.java
+++ src/main/java/io/github/iiiii7d/zylang/Main.java
@@ -3,6 +3,10 @@ package io.github.iiiii7d.zylang;
 import java.util.Arrays;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+
+// for my reference
+//String content = Files.readString(Path.of("test"), StandardCharsets.US_ASCII);
 
 public class Main {
     public static void main(String[] args) {
